cf95acf6e1b7633fbeb3285528e388d057fa4fa4
Nick Firme <nickfirme@Nicks-MacBook-Air.local>
2018-09-23 17:24:41 -0700

    Wrote code to calculate Mars/Jupiter weight

--- planets.py
+++ planets.py
@@ -1,7 +1,16 @@
 def weight_on_planets():
-   # write your code here
-   
-   
-   
+    weight = input("What is your weight on earth? ")
+    earthWeight = int(weight)
+    marsWeight = earthWeight * 0.38
+    jupWeight = earthWeight * 2.34
+
+    print()
+    print("On Mars you would weigh " + str(marsWeight) + " pounds.")
+    print("On Jupiter you would weigh " + str(jupWeight) + " pounds.")
+
+
+
+
+
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
